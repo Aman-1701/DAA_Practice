d9043b9d484a8afc78ceece7e03e67f943bb3e91
AMAN VERMA <67643621+Aman-1701@users.noreply.github.com>
2021-04-17 00:32:19 +0530

    Add files via upload

--- LCS_TD.cpp
+++ LCS_TD.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int LongestCommonSubsequence(string a, string b, int n, int m)
+int LongestCommonSubsequence(string a, string b, int m, int n)
 {
     int i,j;
-    int dp[n+1][m+1];
-    memset(dp,0,sizeof(dp));
+    int dp[m+1][n+1];
+    //memset(dp,0,sizeof(dp));
     
-    for(i=1;i<n+1;i++)
+    for(i=0;i<m+1;i++)
     {
-        for(j=1;j<m+1;j++)
+        for(j=0;j<n+1;j++)
         {
-            if(a[i]==b[j])
+            if (i == 0 || j == 0) 
+                dp[i][j] = 0;
+            else if(a[i-1]==b[j-1])
             {
                 dp[i][j] = 1 + dp[i-1][j-1];
             }
@@ -21,13 +23,15 @@ int LongestCommonSubsequence(string a, string b, int n, int m)
             }
         }
     }
-    return dp[n][m];
+    return dp[m][n];
 }
 int main(int argv, char *argc[])
 {
     string a, b;
-    a = "abcdghty";
-    b = "abedfhtg";
+    a = "heap";
+    b = "pea";
+    cout<<a.length();
+        cout<<b.length();
 
     cout<<LongestCommonSubsequence(a, b, a.length(), b.length());
     return (0);
