153a01a5c6cac381b3b125fe5a904f25428b6c00
AMAN VERMA <67643621+Aman-1701@users.noreply.github.com>
2021-04-18 23:12:39 +0530

    Add files via upload

---
+++ MCM_recursive.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int arr[], int i, int j)
+{
+    int k, temp, max = INT_MAX;
+    if (i >= j)
+    {
+        return 0;
+    }
+    for (k = i; k < j ; k++)
+    {
+        temp = solve(arr, i, k) + solve(arr, k + 1, j) + arr[i - 1] * arr[k] * arr[j];
+        if (max > temp)
+        {
+            max = temp;
+        }
+    }
+    return (max);
+}
+
+int MCM(int arr[], int n)
+{
+    int i, j;
+    i = 1;
+    j = n - 1;
+    return solve(arr, i, j);
+}
+
+int main(int argv, char *argc[])
+{
+    // A1 = 10*20 A2 = 20*30 A3 =30*10 A4 = 10*30
+    int arr[] = {1, 2, 3, 4, 3};
+    int n = sizeof(arr) / sizeof(int);
+
+    cout << MCM(arr, n);
+    return (0);
+}
